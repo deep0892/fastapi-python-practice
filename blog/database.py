ef1b07721d257ea9855e87a8d827e85d55e3bf4b
Deepankar <deepankar080892@gmail.com>
2021-08-13 12:30:52 +0530

    Create model and Tables

--- blog/database.py
+++ blog/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from slqalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABSE_URL = 'sqlite:///./blog.db'
 
--- blog/main.py
+++ blog/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 from typing import Optional
-from . import schemas
+from . import schemas, models
+from .database import engine
 
 import uvicorn
 
 
 app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
 
 
 @app.post('/blog')
